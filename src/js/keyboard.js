8ff6e0e0c3a3a4c467c7159a161f9dd64b7c5ec1
Ilya Fakhrutdinov <bekamez@gmail.com>
2023-04-28 21:26:36 +0500

    feat: add text output element, handle keyboard and mouse events
    - move constants to separate file
    - move language layout to separate file

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,204 +1,11 @@
-const keyOrder = [
-  'Backquote',
-  'Digit1',
-  'Digit2',
-  'Digit3',
-  'Digit4',
-  'Digit5',
-  'Digit6',
-  'Digit7',
-  'Digit8',
-  'Digit9',
-  'Digit0',
-  'Minus',
-  'Equal',
-  'Backspace',
-  'Tab',
-  'KeyQ',
-  'KeyW',
-  'KeyE',
-  'KeyR',
-  'KeyT',
-  'KeyY',
-  'KeyU',
-  'KeyI',
-  'KeyO',
-  'KeyP',
-  'BracketLeft',
-  'BracketRight',
-  'Backslash',
-  'CapsLock',
-  'KeyA',
-  'KeyS',
-  'KeyD',
-  'KeyF',
-  'KeyG',
-  'KeyH',
-  'KeyJ',
-  'KeyK',
-  'KeyL',
-  'Semicolon',
-  'Quote',
-  'Enter',
-  'ShiftLeft',
-  'KeyZ',
-  'KeyX',
-  'KeyC',
-  'KeyV',
-  'KeyB',
-  'KeyN',
-  'KeyM',
-  'Comma',
-  'Period',
-  'Slash',
-  'ArrowUp',
-  'ShiftRight',
-  'ControlLeft',
-  'OSLeft',
-  'AltLelt',
-  'Space',
-  'AltRight',
-  'ArrowLeft',
-  'ArrowDown',
-  'ArrowRight',
-  'ControlRight',
-];
-
-const enKeyLayout = {
-  Backquote: '~',
-  Digit1: '1',
-  Digit2: '2',
-  Digit3: '3',
-  Digit4: '4',
-  Digit5: '5',
-  Digit6: '6',
-  Digit7: '7',
-  Digit8: '8',
-  Digit9: '9',
-  Digit0: '0',
-  Minus: '-',
-  Equal: '=',
-  Backspace: 'Backspace',
-  Tab: 'Tab',
-  KeyQ: 'Q',
-  KeyW: 'W',
-  KeyE: 'E',
-  KeyR: 'R',
-  KeyT: 'T',
-  KeyY: 'Y',
-  KeyU: 'U',
-  KeyI: 'I',
-  KeyO: 'O',
-  KeyP: 'P',
-  BracketLeft: '[',
-  BracketRight: ']',
-  Backslash: '\\',
-  CapsLock: 'CapsLock',
-  KeyA: 'A',
-  KeyS: 'S',
-  KeyD: 'D',
-  KeyF: 'F',
-  KeyG: 'G',
-  KeyH: 'H',
-  KeyJ: 'J',
-  KeyK: 'K',
-  KeyL: 'L',
-  Semicolon: ';',
-  Quote: '\'',
-  Enter: 'Enter',
-  ShiftLeft: 'Shift',
-  KeyZ: 'Z',
-  KeyX: 'X',
-  KeyC: 'C',
-  KeyV: 'V',
-  KeyB: 'B',
-  KeyN: 'N',
-  KeyM: 'M',
-  Comma: ',',
-  Period: '.',
-  Slash: '/',
-  ArrowUp: '🡅',
-  ShiftRight: 'Shift',
-  ControlLeft: 'Ctrl',
-  OSLeft: 'Win',
-  AltLelt: 'Alt',
-  Space: 'Space',
-  AltRight: 'Alt',
-  ArrowLeft: '🡄',
-  ArrowDown: '🡇',
-  ArrowRight: '🡆',
-  ControlRight: 'Ctrl',
-};
-
-const keySizes = {
-  Backquote: 2,
-  Digit1: 2,
-  Digit2: 2,
-  Digit3: 2,
-  Digit4: 2,
-  Digit5: 2,
-  Digit6: 2,
-  Digit7: 2,
-  Digit8: 2,
-  Digit9: 2,
-  Digit0: 2,
-  Minus: 2,
-  Equal: 2,
-  Backspace: 4,
-  Tab: 3,
-  KeyQ: 2,
-  KeyW: 2,
-  KeyE: 2,
-  KeyR: 2,
-  KeyT: 2,
-  KeyY: 2,
-  KeyU: 2,
-  KeyI: 2,
-  KeyO: 2,
-  KeyP: 2,
-  BracketLeft: 2,
-  BracketRight: 2,
-  Backslash: 3,
-  CapsLock: 4,
-  KeyA: 2,
-  KeyS: 2,
-  KeyD: 2,
-  KeyF: 2,
-  KeyG: 2,
-  KeyH: 2,
-  KeyJ: 2,
-  KeyK: 2,
-  KeyL: 2,
-  Semicolon: 2,
-  Quote: 2,
-  Enter: 4,
-  ShiftLeft: 4,
-  KeyZ: 2,
-  KeyX: 2,
-  KeyC: 2,
-  KeyV: 2,
-  KeyB: 2,
-  KeyN: 2,
-  KeyM: 2,
-  Comma: 2,
-  Period: 2,
-  Slash: 2,
-  ArrowUp: 2,
-  ShiftRight: 4,
-  ControlLeft: 3,
-  OSLeft: 2,
-  AltLelt: 2,
-  Space: 13,
-  AltRight: 2,
-  ArrowLeft: 2,
-  ArrowDown: 2,
-  ArrowRight: 2,
-  ControlRight: 2,
-};
+import { keyOrder, keySizes, validKeys } from './constants.js';
+import { enLayout } from './layouts/en.js';
 
 export class Keyboard {
   constructor(container) {
     this.container = container;
+    this.keyboard = navigator.keyboard;
+    this.cursorPos = 0;
   }
 
   init() {
@@ -209,6 +16,10 @@ export class Keyboard {
   buildMarkup() {
     this.keyElements = {};
     this.container.classList.add('keyboard');
+
+    const output = document.createElement('textarea');
+    output.classList.add('keyboard__output');
+    this.output = output;
 
     const keyList = document.createElement('ul');
     keyList.classList.add('keyboard__list');
@@ -226,7 +37,7 @@ export class Keyboard {
       btn.type = 'button';
       btn.dataset.code = keyCode;
       btn.classList.add('keyboard__btn');
-      btn.textContent = enKeyLayout[keyCode];
+      btn.textContent = enLayout[keyCode];
 
       keyEl.append(btn);
       keyList.append(keyEl);
@@ -234,27 +45,75 @@ export class Keyboard {
       this.keyElements[keyCode] = btn;
     });
 
+    this.container.append(output);
     this.container.append(keyList);
+    output.focus();
+  }
+
+  handleKeyEvent(e) {
+    this.keyElements[e.code].classList.add('keyboard__btn--active');
+
+    if (validKeys.includes(e.code)) {
+      return;
+    }
+
+    this.cursorPos = this.output.selectionEnd;
+    this.output.setRangeText(
+      e.key,
+      this.cursorPos,
+      this.cursorPos,
+      'end',
+    );
   }
 
   bindEvents() {
+    // Mouse events
     this.container.addEventListener('mousedown', (e) => {
-      if (e.target.dataset.code) {
-        e.target.classList.add('keyboard__btn--active');
+      this.cursorPos = this.output.selectionEnd;
+
+      const { code } = e.target.dataset;
+      if (!code) {
+        return;
       }
+
+      e.preventDefault();
+      e.target.classList.add('keyboard__btn--active');
+
+      this.keyboard.getLayoutMap().then((keyboardLayoutMap) => {
+        this.output.focus();
+
+        const keyboardEvent = new KeyboardEvent('keydown', {
+          key: keyboardLayoutMap.get(code),
+          code,
+        });
+
+        this.handleKeyEvent(keyboardEvent);
+      });
     });
 
     this.container.addEventListener('mouseup', () => {
       keyOrder.forEach((keyCode) => this.keyElements[keyCode].classList.remove('keyboard__btn--active'));
     });
 
+    // Keyboard events
     window.addEventListener('keydown', (e) => {
-      e.preventDefault();
-      this.keyElements[e.code].classList.add('keyboard__btn--active');
+      this.output.focus();
+      this.cursorPos = this.output.selectionEnd;
+
+      if (!validKeys.includes(e.code)) {
+        e.preventDefault();
+      }
+      if (!keyOrder.includes(e.code)) {
+        return;
+      }
+
+      this.handleKeyEvent(e);
     });
 
     window.addEventListener('keyup', (e) => {
-      e.preventDefault();
+      if (!keyOrder.includes(e.code)) {
+        return;
+      }
       this.keyElements[e.code].classList.remove('keyboard__btn--active');
     });
   }
